a290d9939971d182a0063ab454cffaebe70569c1
BrenoCRocha08 <breno.caetano.3003@gmail.com>
2026-02-23 22:20:00 -0300

    Add files via upload

---
+++ kmcorridos.py
@@ -0,0 +1,4 @@
+kms=float(input("Quantos km você rodou com o carro? "))
+dia=float(input("Por quantos dias você alugou o carro? "))
+preco=(dia*60)+(kms*0.15)
+print("O valor total a se pagar é de: ", preco)
